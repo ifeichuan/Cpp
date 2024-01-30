5d51d1d298c8c19910857c1761f969eccf3df7c7
Feichuan <Feichuan05@gamil.com>
2024-01-30 16:44:31 +0800

    P1177

---
+++ Other/P1177_luogu.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+long long n;
+int main(){
+    scanf("%lld",&n);
+    long long nums[n];
+    for(long long i=0;i<n;i++){
+        scanf("%lld",&nums[i]);
+    }
+    sort(nums,nums+n);
+    printf("%lld",nums[0]);
+    for(long long i=1;i<n;i++){
+        printf(" %lld",nums[i]);
+    }
+    printf("\n");
+}
