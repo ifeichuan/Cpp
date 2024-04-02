591c4402d47a80ec1ea67ba92154cdeff7280402
Feichuan <Feichuan05@gamil.com>
2024-04-02 20:05:48 +0800

    add

--- Other/P1255_数楼梯.cpp
+++ Other/P1255_数楼梯.cpp
@@ -2,22 +2,9 @@
 
 using namespace std;
 
-int recursion(int n){
-    if(n==1){
-        return 1;
-    }
-    else if(n==2){
-        return 2;
-    }
-    else{
-        return recursion(n-1)+recursion(n-2);
-    }
+int main(){
+    int n;
+    cin>>n;
+    cout<<pow(2,n)-n;
 }
 
-int main(){
-    int a;
-    cin>>a;
-    if(a==0) cout<<0;
-    else{
-    cout<<recursion(a);}
-}
